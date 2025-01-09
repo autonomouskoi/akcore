b7bc6ee2dfa2a59e88c1661fa46f47d70ccf3bfd
Jason Mansfield <jason@lub-dub.org>
2025-01-08 19:15:26 -0800

    remove VERSION, fix misplaced run files

--- ak.go
+++ ak.go
@@ -3,21 +3,15 @@ package akcore
 import (
 	_ "embed"
 	"errors"
-	"strings"
 
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
-//go:embed VERSION
-var Version string
+var Version = "DEV"
 
 var (
 	ErrNotFound = errors.New("not found")
 )
-
-func init() {
-	Version = strings.TrimSpace(Version)
-}
 
 type ProtoMessagePointer[M any] interface {
 	*M
